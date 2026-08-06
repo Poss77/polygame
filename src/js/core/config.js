8d54afecfff2c7c362482888129621495e41518f
Pascal Dufour <pascaldufour@gmail.com>
2026-08-06 13:28:29 -0400

    v1.4.330: Sync default HTML version display tag in index.html to v1.4.330

--- src/js/core/config.js
+++ src/js/core/config.js
@@ -9,7 +9,7 @@ export const WALLETCONNECT_PROJECT_ID = "00950c9a536e980dd84dbc015411baa7";
 export const VAULT_RECEIVER_ADDRESS = "0x10B9993990c9EF8a212c9557cB02aD94da9a654d"; // 50% Treasury Pool (Master Admin)
 export const BURN_RECEIVER_ADDRESS = "0x000000000000000000000000000000000000dEaD"; // 50% Deflationary Burn
 export const ADMIN_WALLET_ADDRESS = "0x10B9993990c9EF8a212c9557cB02aD94da9a654d";
-export const APP_VERSION = "1.4.329"; // Force unregister stale SW & update script tag version query params to v=1.4.329
+export const APP_VERSION = "1.4.330"; // Update HTML default version tag & script query params to v1.4.330
 
 export let web3Provider = null;
 export let realSigner = null;
