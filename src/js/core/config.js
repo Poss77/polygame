e392754a1d1585b1723b1dd54db01980843a26d7
Pascal Dufour <pascaldufour@gmail.com>
2026-08-31 18:00:08 -0400

    Update APP_VERSION constant to 1.5.221 in config.js

--- src/js/core/config.js
+++ src/js/core/config.js
@@ -9,7 +9,7 @@ export const WALLETCONNECT_PROJECT_ID = "00950c9a536e980dd84dbc015411baa7";
 export const ADMIN_WALLET_ADDRESS = "0x10B9993990c9EF8a212c9557cB02aD94da9a654d"; // Master Admin Wallet
 export const VAULT_RECEIVER_ADDRESS = "0x10B9993990c9EF8a212c9557cB02aD94da9a654d"; // 50% Treasury Pool (Master Admin)
 export const BURN_RECEIVER_ADDRESS = "0x000000000000000000000000000000000000dEaD"; // 50% Deflationary Burn
-export const APP_VERSION = "1.5.216"; // Serie 1 Apex Relics 1.5x Multiplier Faucet & Arcade Fix
+export const APP_VERSION = "1.5.221"; // Modular Weekly Reset Suite (4 Steps + Master Pipeline)
 
 export let web3Provider = null;
 export let realSigner = null;
